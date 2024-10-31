2e4d4fe0456ab73b4127a37b542a6f73e3ddbcaa
김태훈 <zxader@naver.com>
2024-10-31 20:24:09 +0900

    feat: 오픈 비두 기능 구현 세션 아이디 발급 토큰 발급

--- back/src/main/java/com/ssafy/daily/quiz/controller/QuizController.java
+++ back/src/main/java/com/ssafy/daily/quiz/controller/QuizController.java
@@ -1,13 +1,19 @@
 package com.ssafy.daily.quiz.controller;
 
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import com.ssafy.daily.quiz.dto.CheckWordRequest;
 import com.ssafy.daily.quiz.dto.SetWordRequest;
 import com.ssafy.daily.quiz.service.QuizService;
 import com.ssafy.daily.user.dto.CustomUserDetails;
+import io.openvidu.java.client.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,23 +21,21 @@ import org.springframework.web.bind.annotation.*;
 public class QuizController {
     private final QuizService quizService;
 
+    // 세션 아이디 생성
+    @PostMapping("/sessions")
+    public ResponseEntity<?> initializeSession(@RequestBody(required = false) Map<String, Object> params)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+        return ResponseEntity.ok(quizService.initializeSession(params));
+    }
+
     // 토큰 생성
-    @GetMapping( "/token")
-    public ResponseEntity<?> createToken(@AuthenticationPrincipal CustomUserDetails userDetails) throws Exception {
-        return ResponseEntity.ok(quizService.createToken(userDetails));
+    @PostMapping("/sessions/{sessionId}/connections")
+    public ResponseEntity<?> createConnection(@PathVariable("sessionId") String sessionId,
+                                              @RequestBody(required = false) Map<String, Object> params)
+            throws OpenViduJavaClientException, OpenViduHttpException {
+        return ResponseEntity.ok(quizService.createConnection(sessionId, params));
     }
-    
-    // 웹훅
-    @PostMapping(value = "/livekit/webhook", consumes = "application/webhook+json")
-    public ResponseEntity<String> receiveWebhook(@RequestHeader("Authorization") String authHeader, @RequestBody String body) {
-        try {
-            quizService.receiveWebhook(authHeader, body);
-        } catch (Exception e) {
-            System.err.println("Error validating webhook event: " + e.getMessage());
-        }
-        return ResponseEntity.ok("Webhook received");
-    }
-    
+
     // 단어 추천
     @GetMapping("/word/recommend")
     public ResponseEntity<?> recommendWord(@AuthenticationPrincipal CustomUserDetails userDetails) {
--- back/src/main/java/com/ssafy/daily/quiz/service/QuizService.java
+++ back/src/main/java/com/ssafy/daily/quiz/service/QuizService.java
@@ -1,7 +1,6 @@
 package com.ssafy.daily.quiz.service;
 
 import com.ssafy.daily.alarm.service.AlarmService;
-import com.ssafy.daily.common.Role;
 import com.ssafy.daily.quiz.dto.CheckWordRequest;
 import com.ssafy.daily.quiz.dto.RecommendWordResponse;
 import com.ssafy.daily.quiz.dto.SetWordRequest;
@@ -12,15 +11,15 @@ import com.ssafy.daily.word.entity.LearnedWord;
 import com.ssafy.daily.word.entity.Word;
 import com.ssafy.daily.word.repository.LearnedWordRepository;
 import com.ssafy.daily.word.repository.WordRepository;
-import io.livekit.server.AccessToken;
-import io.livekit.server.RoomJoin;
-import io.livekit.server.RoomName;
-import io.livekit.server.WebhookReceiver;
-import livekit.LivekitWebhook;
+import io.openvidu.java.client.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.annotation.PostConstruct;
 import java.util.*;
 
 @RequiredArgsConstructor
@@ -31,39 +30,37 @@ public class QuizService {
     private final QuizRepository quizRepository;
     private final AlarmService alarmService;
 
-    @Value("${livekit.api.key}")
-    private String LIVEKIT_API_KEY;
+    @Value("${openvidu.url}")
+    private String OPENVIDU_URL;
 
-    @Value("${livekit.api.secret}")
-    private String LIVEKIT_API_SECRET;
+    @Value("${openvidu.secret}")
+    private String OPENVIDU_SECRET;
+
+    private OpenVidu openvidu;
+
+    @PostConstruct
+    public void init() {
+        this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
+
+    // 세션 아이디 생성
+    public String initializeSession(Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
+        SessionProperties properties = SessionProperties.fromJson(params).build();
+        Session session = openvidu.createSession(properties);
+        return session.getSessionId();
+    }
 
     // 토큰 생성
-    public Map<String, String> createToken(CustomUserDetails userDetails) throws Exception {
-        int familyId = userDetails.getFamily().getId();
-        String roomName = "quizRoom" + familyId;
-        String participantName = (userDetails.getMember() != null)
-                ? userDetails.getMember().getName()
-                : userDetails.getFamily().getUsername();
-
-        AccessToken token = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
-
-        token.setName(participantName);
-        token.setIdentity(participantName);
-        token.addGrants(new RoomJoin(true), new RoomName(roomName));
-        if(userDetails.getMember() != null) {
-            alarmService.sendNotification(participantName, 0, familyId, Role.PARENT, "그림 퀴즈", "요청");
+    public String createConnection(String sessionId, Map<String, Object> params) throws OpenViduJavaClientException, OpenViduHttpException {
+        Session session = openvidu.getActiveSession(sessionId);
+        if (session == null) {
+          throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Session not found");
         }
-
-        return Map.of("token", token.toJwt());
+        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+        Connection connection = session.createConnection(properties);
+        return connection.getToken();
     }
 
-    // 웹 훅
-    public void receiveWebhook(String authHeader, String body) {
-        WebhookReceiver webhookReceiver = new WebhookReceiver(LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
-        LivekitWebhook.WebhookEvent event = webhookReceiver.receive(body, authHeader);
-        System.out.println("LiveKit Webhook: " + event.toString());
-    }
-    
     // 단어 추천
     public List<RecommendWordResponse> recommendWord(CustomUserDetails userDetails) {
         int memberId = userDetails.getMember().getId();
@@ -103,4 +100,7 @@ public class QuizService {
         Quiz quiz = quizRepository.findByFamilyId(familyId);
         return request.getWord().equals(quiz.getWord());
     }
+
+
+
 }
