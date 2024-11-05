4741094828af57d47b39126e40bd1e426b2efee8
이승지 <esg0107@gmail.com>
2024-11-05 20:43:56 +0900

    feat: 부모가 아이의 학습한 단어 조회하는 로직 추가

--- back/src/main/java/com/ssafy/daily/word/controller/LearnedWordController.java
+++ back/src/main/java/com/ssafy/daily/word/controller/LearnedWordController.java
@@ -1,15 +1,19 @@
-// src/main/java/com/ssafy/daily/word/controller/LearnedWordController.java
 package com.ssafy.daily.word.controller;
 
+import com.ssafy.daily.user.entity.Member;
+import com.ssafy.daily.user.repository.MemberRepository;
 import com.ssafy.daily.word.dto.LearnedWordResponse;
 import com.ssafy.daily.word.service.WordService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import com.ssafy.daily.user.dto.CustomUserDetails;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/word/learned")
@@ -17,9 +21,10 @@ import java.util.List;
 public class LearnedWordController {
 
     private final WordService wordService;
+    private final MemberRepository memberRepository;
 
     @GetMapping
-    public ResponseEntity<List<LearnedWordResponse>> getLearnedWordsByMember(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ResponseEntity<List<LearnedWordResponse>> getOwnLearnedWords(@AuthenticationPrincipal CustomUserDetails userDetails) {
         int memberId = userDetails.getMemberId();
         List<LearnedWordResponse> learnedWords = wordService.getLearnedWordsByMember(memberId);
 
@@ -28,5 +33,31 @@ public class LearnedWordController {
         }
 
         return ResponseEntity.ok(learnedWords);
+    }
+
+    @GetMapping("/{childId}")
+    public ResponseEntity<List<LearnedWordResponse>> getChildLearnedWords(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable int childId) {
+        Optional<Member> optionalMember = memberRepository.findById(childId);
+
+        if (optionalMember.isPresent()) {
+            Member child = optionalMember.get();
+            int childFamilyId = child.getFamily().getId();
+            int parentFamilyId = userDetails.getFamilyId();
+            int parentId = userDetails.getMemberId();
+
+            if (parentFamilyId == childFamilyId && parentId == 0) {
+                List<LearnedWordResponse> learnedWords = wordService.getLearnedWordsByMember(childId);
+
+                if (learnedWords.isEmpty()) {
+                    return ResponseEntity.noContent().build();
+                }
+
+                return ResponseEntity.ok(learnedWords);
+            } else {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Collections.emptyList());
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
