f4e6ae3aa7574a5087e70050342dffc090cb5e9a
권정솔 <rnjsjsol0121@gmail.com>
2024-11-06 00:54:47 +0900

    feat: 단어 유사도 ocr - OCR로 단어학습 유사도 검사

--- back/src/main/java/com/ssafy/daily/word/controller/LearningSessionController.java
+++ back/src/main/java/com/ssafy/daily/word/controller/LearningSessionController.java
@@ -44,8 +44,9 @@ public class LearningSessionController {
     }
 
     @PostMapping("/validate")
-    public ResponseEntity<?> checkWordSimilarity(@RequestPart MultipartFile orgFile, @RequestPart MultipartFile writeFile){
-        wordService.checkSimilarity(orgFile, writeFile);
+    public ResponseEntity<?> checkWordSimilarity(@RequestPart MultipartFile writeFile,
+                                                 @RequestPart String word){
+        wordService.checkSimilarity(word, writeFile);
         return ResponseEntity.ok("단어가 정확하게 작성되었습니다!");
     }
 }
