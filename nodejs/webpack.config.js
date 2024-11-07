4dce969c4d54591416dc2e9a97f616a8eb7ad931
김동환 <ehdghks9719@naver.com>
2024-11-07 16:44:00 +0900

    refactor : dockerfile 수정

--- nodejs/webpack.config.js
+++ nodejs/webpack.config.js
@@ -1,18 +1,32 @@
+const nodeExternals = require("webpack-node-externals");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js", // 프로젝트의 실제 엔트리 파일 경로로 설정
+  mode: "production",
+  entry: {
+    bundle: path.resolve(__dirname, "index.js"),
+  },
   output: {
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
   },
-  target: "node", // Node.js 환경에서 실행되도록 설정
-  externals: {
-    fs: "commonjs fs",
-    net: "commonjs net",
-    async_hooks: "commonjs async_hooks",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
   },
-  resolve: {
-    extensions: [".js", ".json"],
+  target: "node",
+  externalsPresets: {
+    node: true,
   },
+  externals: [nodeExternals()],
 };
