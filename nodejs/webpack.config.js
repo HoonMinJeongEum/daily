9bb4cc1cf683cf9d39c9682380886967ffdb6a91
김동환 <ehdghks9719@naver.com>
2024-11-07 14:02:53 +0900

    refactor : webpack 수정

--- nodejs/webpack.config.js
+++ nodejs/webpack.config.js
@@ -1,7 +1,18 @@
-export default {
-  entry: "./src/index.js",
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", // 프로젝트의 실제 엔트리 파일 경로로 설정
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  target: "node", // Node.js 환경에서 실행되도록 설정
+  externals: {
+    fs: "commonjs fs",
+    net: "commonjs net",
+    async_hooks: "commonjs async_hooks",
+  },
+  resolve: {
+    extensions: [".js", ".json"],
   },
 };
