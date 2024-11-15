f1124651d5a552e6a483f20ce02d6e33c1119348
김태훈 <zxader@naver.com>
2024-11-15 10:12:18 +0900

    fix: 이름변경하면 알림 이름 수정

--- back/src/main/java/com/ssafy/daily/alarm/repository/AlarmRepository.java
+++ back/src/main/java/com/ssafy/daily/alarm/repository/AlarmRepository.java
@@ -3,9 +3,11 @@ package com.ssafy.daily.alarm.repository;
 import com.ssafy.daily.alarm.entity.Alarm;
 import com.ssafy.daily.alarm.entity.FCMToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,4 +22,13 @@ public interface AlarmRepository extends JpaRepository<Alarm, Long> {
             "a.confirmedAt DESC")
     List<Alarm> findByFcmTokenIdWithSorting(@Param("fcmTokenId") int fcmTokenId);
     void deleteByNameAndFcmToken_Id(String name, int fcmTokenId);
+    @Modifying
+    @Transactional
+    @Query("UPDATE Alarm a SET a.name = :newName WHERE a.name = :oldName AND a.fcmToken.id = :fcmTokenId")
+    void updateAlarmNameByFcmToken(
+            @Param("oldName") String oldName,
+            @Param("newName") String newName,
+            @Param("fcmTokenId") int fcmTokenId
+    );
+
 }
--- back/src/main/java/com/ssafy/daily/user/service/UserService.java
+++ back/src/main/java/com/ssafy/daily/user/service/UserService.java
@@ -290,6 +290,9 @@ public class UserService {
         }
 
         int memberId = userDetails.getMemberId();
+
+        FCMToken fcmToken = alarmService.getToken(userDetails.getFamilyId(), Role.PARENT);
+        alarmRepository.updateAlarmNameByFcmToken(userDetails.getMember().getName(), newName, fcmToken.getId());
         Member orgMember = memberRepository.findById(memberId)
                 .orElseThrow(() -> new EmptyResultDataAccessException("해당 프로필을 찾을 수 없습니다.", 1));
 
