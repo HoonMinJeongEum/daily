60402abcd3c89d2fd8257cae8420a5a2c5cd8812
김태훈 <zxader@naver.com>
2024-11-16 19:19:58 +0900

    fix: 알림 수정

--- back/src/main/java/com/ssafy/daily/alarm/service/AlarmService.java
+++ back/src/main/java/com/ssafy/daily/alarm/service/AlarmService.java
@@ -74,6 +74,7 @@ public class AlarmService {
                         .setTitle(title)
                         .setBody(body)
                         .build())
+                .putData("title", title)
                 .putData("titleId", titleId)
                 .putData("name", name)
                 .build();
